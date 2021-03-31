18ff102ead611abb14b6c4ebe41be9e7f0ef1ec9
Junyu <yukuroiame@gmail.com>
2021-03-31 09:08:52 -0300

    Primeiro commit do projeto Spring boot microservices

---
+++ src/main/java/com/programming/techie/productservice/controller/ProductController.java
@@ -0,0 +1,39 @@
+package com.programming.techie.productservice.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.programming.techie.productservice.model.Product;
+import com.programming.techie.productservice.repository.ProductRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+	
+	private final ProductRepository productRepository;
+
+	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
+	public List<Product> findAll() {
+		return productRepository.findAll();
+
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public void createProduct(@RequestBody Product product) {
+		productRepository.save(product);
+
+	}
+
+}
